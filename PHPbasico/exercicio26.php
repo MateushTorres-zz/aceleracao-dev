26ca8a062add0f5d53346cdbd228ed909f910768
MateushTorres <61062377+MateushTorres@users.noreply.github.com>
2021-12-12 18:06:56 -0300

    Update exercicio26.php

--- PHPbasico/exercicio26.php
+++ PHPbasico/exercicio26.php
@@ -1,17 +1,21 @@
 <?php
+$var1 = random_int(0,99);
+$var2 = random_int(0,99);
+$var3 = random_int(0,99);
 
-$v1 = random_int(0,99);
-$v2 = random_int(0,99);
-$v3 = random_int(0,99);
-
-while ($v1 <= 100 && $v2 <= 100 && $v3 <= 100) {
-    if($v1 % 2 === 0) echo $v1 . "<br>";
-    $v1++;
-
-    if($v2 % 2 === 0) echo $v2 . "<br>";
-    $v2++;
-
-    if($v3 % 2 === 0) echo $v3 . "<br>";
-    $v3++;
+function verif($valor)
+{
+    while(true){
+        if($valor%2 == 0){
+            echo "$valor <br>";
+        }else{
+            $valor +=1;
+            echo "$valor <br>";
+        }
+        break;
+    }
 }
+verif($var1);
+verif($var2);
+verif($var3);
 ?>
