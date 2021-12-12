4f974d7968d97547a6c6c76ae6baeda4db8f02ff
MateushTorres <61062377+MateushTorres@users.noreply.github.com>
2021-12-12 18:40:11 -0300

    Update exercicio29.php

--- PHPbasico/exercicio29.php
+++ PHPbasico/exercicio29.php
@@ -1,7 +1,5 @@
 <?php
-
-$meso = file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/mesorregioes/1101");
-$array = array($meso);
+$mesorregioes = file_get_contents("https://servicodados.ibge.gov.br/api/v1/localidades/mesorregioes/1101");
+$array = array($mesorregioes);
 var_export($array);
-
 ?>
