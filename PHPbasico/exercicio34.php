6d91ac8c7dd0bbfdb7129f23b7a7591376bcb366
MateushTorres <61062377+MateushTorres@users.noreply.github.com>
2021-12-13 23:14:18 -0300

    Update exercicio34.php

--- PHPbasico/exercicio34.php
+++ PHPbasico/exercicio34.php
@@ -1,8 +1,13 @@
 <?php
-    $a = [0,10,20,30,40,50,60,70,80,90];
 
-    for($i = sizeof($a) - 1; $i > 0; $i--){
-        echo "<li>" . $a[$i] . "</li>";
-        echo '<br>';
+$valores = [0,20,30,40,50,60,70,80,90];
+
+function Inverso($array)
+{
+    for ($i= count($array); $i >= 0 ; $i--) { 
+        echo "<br>" . $array[$i];
     }
+}
+
+Inverso($valores);
 ?>
