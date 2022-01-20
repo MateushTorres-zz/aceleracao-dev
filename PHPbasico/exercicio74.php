86ab5b5c8a1034822e34c971cc8f07ca9eb144da
MateushTorres <61062377+MateushTorres@users.noreply.github.com>
2022-01-19 23:34:19 -0300

    Update exercicio74.php

--- PHPbasico/exercicio74.php
+++ PHPbasico/exercicio74.php
@@ -1,13 +1,44 @@
 <?php
-function x(array $correntistas) {
-    $despesas = [];
+class Correntista{
+    private string $cpf;
+    private float $saldo;
+    public function __construct(string $cpf, float $saldo){
+        $this->cpf = $cpf;
+        $this->saldo = $saldo;
+    }
+    public function getcpf(): string{
+        return $this->cpf;
+    }
+
+    public function getSaldo(): float{
+        return $this->saldo;
+    }
+    public function setSaldo(float $saldo): void{
+        $this->saldo = $saldo;
+    }
+}
+class Despesa extends Correntista{
+    private array $despesasPrev;
+    public function __construct( string $cpf, float $saldo, array $despesasPrev){
+        parent::__construct($cpf, $saldo);
+        $this->despesasPrev = $despesasPrev;
+    }
+    public function getDespesasPrev(): array{
+        return $this->despesasPrev;
+    }
+}
+function verifica (array $correntistas): array{
+    $correntistas = [];
+
     foreach ($correntistas as $correntista) {
-        if ($correntista->getMes() === 3) {
-            if($correntista->getSaldo() <= $correntista->getDespesasPrevistas()) {
-                $despesas[] = $correntista;
-            }
+        $tot = 0;
+
+        foreach ($correntista->getDespesasPrev() as $despesaPrev) {
+            $tot += $despesaPrev->getValor();
+        }
+        if ($correntista->getSaldo() > $tot) {
+            $correntistas[] = $correntista;
         }
     }
-    return $despesas;
-}
+    return $correntistas;
 ?>
